bfee4ec5f9f25d1db87984798669dac0b5a95599
Flosrn <flosrn.dev@gmail.com>
2023-07-11 15:36:55 +0200

    build files

--- libs/discord.ws.js
+++ libs/discord.ws.js
@@ -212,7 +212,7 @@ class WsMessage {
             return;
         }
         const message = msg.d;
-        this.log("message event", msg.t);
+        // this.log("message event", msg.t);
         switch (msg.t) {
             case "READY":
                 this.emitSystem("ready", message.user);
